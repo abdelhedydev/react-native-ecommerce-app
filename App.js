da3e2ff03b0263d64b8bc066c93ae2f7f0bcccaf
unknown <abdelhedi.hlel@oyez.fr>
2019-06-17 14:02:59 +0100

    apollo config

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
-import { createAppContainer } from 'react-navigation';
-import Navigator from './Navigator';
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import Layouts from './components/Layouts';
 
-const App = createAppContainer(Navigator);
+const client = new ApolloClient({
+  uri: 'https://api-euwest.graphcms.com/v1/cjuibv7et3llg01cswnhrmphb/master',
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Layouts />
+  </ApolloProvider>
+);
 
 export default App;
---
+++ components/Layouts.js
@@ -0,0 +1,6 @@
+import { createAppContainer } from 'react-navigation';
+import Navigator from '../Navigator';
+
+const Layouts = createAppContainer(Navigator);
+
+export default Layouts;
